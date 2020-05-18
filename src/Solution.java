f6485997e09e5a7e07746d45c9600cb2fecd6094
venustuspl <venustus.pl@gmail.com>
2020-05-18 21:16:12 +0200

    0.3

--- src/Solution.java
+++ src/Solution.java
@@ -3,28 +3,37 @@ import java.util.Set;
 
 class Solution {
     public static int solution(int[] A) {
-            int result1 = 0;
-            int result2 = A.length;
-            Set<Integer> intSet = new HashSet<>();
-            for (int x : A){
-                intSet.add(x);
+        int result1 = 0;
+        int result2 = A.length;
+        Set<Integer> intSet = new HashSet<>();
+        for (int x : A) {
+            intSet.add(x);
+        }
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = i; j < A.length; j++) {
+                if (intSet.contains(A[j])) {
+                    intSet.remove(A[j]);
+                    System.out.print(A[j] + " * ");
+                }
+                result1++;
+                System.out.println("- " + result1 + " -");
+                if (intSet.isEmpty()) {
+                    if (result1 < result2) result2 = result1;
+                    System.out.print("result2 " + result2);
+                    result1 = 0;
+                    for (int x : A) {
+                        intSet.add(x);
+                    }
+                    break;
+                }
             }
 
-            for (int i = 0; i < A.length; i++){
-                for (int j = i; j < A.length; j++) {
-                    if (intSet.contains(A[j])) {
-                        intSet.remove(A[j]);
-                    }
-                    result1++;
-                    if (intSet.isEmpty()) {result1--;
-                        break;}
-                }
-                if (result1 < result2) result2 = result1;
-            }
-result1 = 0;
-            // write your code in Java SE 8
-            return result2;
         }
+
+        // write your code in Java SE 8
+        return result2;
+    }
 
 
     public static void main(String[] args) {
